2308d1eac40624fcca4fac196362b87979f7d1bc
Tridibesh Sastri <88239446+Tridibesh-Sastri@users.noreply.github.com>
2025-08-08 01:50:34 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+let score = '33'
+let code = '33ab'
+let cod = null
+let co = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// console.table([
+//     Number(score),
+//     typeof Number(score),
+//     Number(code),
+//     typeof Number(code),
+//     Number(cod),
+//     typeof Number(cod),
+//     Number(co), 
+//     typeof Number(co)
+// ])
+
+// Number() conversion
+// "22" => 22
+// "33abd" => Nan
+// true => 1
+// null => 0
+// undefined => Nan
+
+let bool = 1
+let positive = 13
+let boo = "1"
+let bo = '13'
+let c = null
+let d = undefined
+let negtive = -3
+let str = 'str'
+let emptyStr = ''
+
+// console.table([typeof bool,typeof Boolean(bool),bool, Boolean(bool)])
+// console.table([typeof positive,typeof Boolean(positive),positive, Boolean(positive)])
+// console.table([typeof boo,typeof Boolean(boo),boo, Boolean(boo)])
+// console.table([typeof bo,typeof Boolean(bo),bo, Boolean(bo)])
+// console.table([typeof c,typeof Boolean(c),c, Boolean(c)])
+// console.table([typeof d,typeof Boolean(d),d, Boolean(d)])
+// console.table([typeof negtive,typeof Boolean(negtive),negtive, Boolean(negtive)])
+// console.table([typeof str,typeof Boolean(str),str, Boolean(str)])
+// console.table([typeof emptyStr,typeof Boolean(emptyStr),emptyStr, Boolean(emptyStr)])
+
+// Boolean() conversion
+// 1 => true
+// 13 => true
+// '13' => true
+// 'string' => true
+// -3 => true
+// '' => false
+// 0 => false
+// null => false
+// undefined => false
+
+
